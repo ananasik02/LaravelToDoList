ba5b8acca5cd56a6022f1d2c9e04c820abb8feb3
nadiia <matsonka02@gmail.com>
2020-12-03 14:18:15 +0200

    login and register system created

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,15 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by TEMPLATED
-http://templated.co
-Released for free under the Creative Commons Attribution License
 
-Name       : SimpleWork
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20140225
-
--->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -36,9 +26,38 @@ Released   : 20140225
                 <ul>
                     <li class="{{Request::path() === "/" ? 'current_page_item' : ' '}}"><a href="/" accesskey="1" title="">Homepage</a></li>
                     <li class="{{Request::path() === "tasks" ? 'current_page_item' : ' '}}"><a href="/tasks" accesskey="2" title="">Tasks</a></li>
-                    <li class="{{Request::path() === "about" ? 'current_page_item' : ' '}}"><a href="/about" accesskey="3" title="">About Us</a></li>
-                    <li class="{{Request::path() === "articles" ? 'current_page_item' : ' '}}"><a href="/articles" accesskey="4" title="">Articles</a></li>
-                    <li class="{{Request::path() === "contact" ? 'current_page_item' : ' '}}"><a href="#" accesskey="5" title="">Contact Us</a></li>
+                    <li class="{{Request::path() === "contact" ? 'current_page_item' : ' '}}"><a href="#" accesskey="3" title="">Contact Us</a></li>
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
                 </ul>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TasksController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-
+Auth::routes();
 
 Route::get('/tasks', 'App\Http\Controllers\TasksController@index')->name('tasks.index');
 Route::post('/tasks', 'App\Http\Controllers\TasksController@store');
@@ -30,3 +16,4 @@ Route::get('/tasks/{task}', 'App\Http\Controllers\TasksController@show')->name('
 Route::get('/tasks/{task}/edit', 'App\Http\Controllers\TasksController@edit');
 Route::put('/tasks/{task}', 'App\Http\Controllers\TasksController@update');
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
