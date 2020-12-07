2b409ddfe2ee052a36ad6f049894b67b42d1c69b
nadiia <matsonka02@gmail.com>
2020-12-07 14:35:05 +0200

    added a delete function

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\Tag;
 use App\Models\User;
+use App\Notifications\TaskAssigned;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
 
 class TasksController extends Controller
 {
@@ -34,15 +37,12 @@ class TasksController extends Controller
 
     public function store()
     {
-        //dd(request()->all());
 
         $task = new Task($this->validateTask());
-        //$task = new Task($this->validateTask());
-        //dd("here");
         $task->user_id = 1;
         $task->save();
         $task->tags()->attach(request('tags'));
-       // Task::create(request()->all());
+        User::find(request()->performer_id)->notify(new TaskAssigned($task->id));
         return redirect(route('tasks.index'));
 
 
@@ -50,8 +50,14 @@ class TasksController extends Controller
 
     public function edit(Task $task)
     {
-        return view('tasks.edit' , ['task'=>$task]);
+        return view('tasks.edit' , [
+            'task'=>$task,
+            'tags' => Tag::all(),
+            'pms' => User::all(),
+            'performers' => User::all()
+        ]);
     }
+
 
     public function update(Task $task)
     {
@@ -61,7 +67,9 @@ class TasksController extends Controller
 
     public function destroy()
     {
-
+        $task = Task::find(\request()->id);
+        $task->destroy(\request()->id);
+        return redirect(route('tasks.index'));
     }
 
     protected function validateTask()
